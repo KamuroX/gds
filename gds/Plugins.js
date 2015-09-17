07230bc64ffe48d2fa0ae2d64f89fda01e6db0fc
= <terrahen@web.de>
2015-09-17 22:13:15 +0200

    start plugins

--- gds/Plugins.js
+++ gds/Plugins.js
@@ -1,4 +1,5 @@
 var plugins = require('fs').readdirSync('./gds');
+var gutil = require('gulp-util').colors;
 
 for (var plugin in plugins) {
   if (plugins[plugin] !== 'Plugins.js') {
@@ -7,7 +8,20 @@ for (var plugin in plugins) {
 }
 
 for (var plugin in module.exports) {
-  if (module.exports[plugin].init !== undefined) {
-    module.exports[plugin].init();
+  if (module.exports[plugin].dependencies !== undefined) {
+    var dependencies = module.exports[plugin].dependencies();
+
+    for (var dependency in dependencies) {
+      if (module.exports[dependencies[dependency]] === undefined) {
+        console.warn(gutil.yellow('Don\'t load "' + plugin + '" plugin cause by missing dependent plugin "' + dependencies[dependency] + '"'));
+        delete module.exports[plugin];
+        break;
+      } else {
+        module.exports[plugin][dependencies[dependency]] = module.exports[dependencies[dependency]];
+      }
+    }
+    if (module.exports[plugin] !== undefined && module.exports[plugin].init !== undefined) {
+      module.exports[plugin].init();
+    }
   }
 }
---
+++ gds/ops.js
@@ -0,0 +1,88 @@
+module.parent.exports.ops = {
+
+  dependencies: function() {
+    return [
+      'jsons',
+      'scan',
+      'tasks',
+      'devel',
+    ];
+  },
+
+  init: function() {
+    Jsons.init();
+    var input = copyObject(argv);
+    var local = {};
+    var def = {};
+
+    delete input['_'];
+    delete input['$0'];
+
+    if (Jsons.local.exist) {
+      local = Jsons.local.data.options;
+    }
+
+    for (var task in Tasks.tasks) {
+      def[task] = Tasks.tasks[task].def();
+    }
+
+    for (var task in def) {
+      for (var op in def[task]) {
+        this.options[task + '-' + op] = Scan.input(def[task][op]);
+      }
+    }
+    for (var global in Tasks.globals) {
+      this.options[global] = Tasks.globals[global].def;
+    }
+    for (var task in local) {
+      if (isObject(local[task])) {
+        for (var op in local[task]) {
+          this.options['local-' + task + '-' + op] = Scan.input(local[task][op]);
+        }
+      } else {
+        this.options['local-' + task] = Scan.input(local[task]);
+      }
+    }
+    for (var op in input) {
+      this.options['user-' + op] = Scan.input(input[op]);
+    }
+
+    Devel.debug(this.options, 'options');
+  },
+
+  options: {},
+
+  searchings: function(name) {
+    return [
+      'user-' + Devel.current() + '-' + name,
+      'user-' + name,
+      'local-' + Devel.current() + '-' + name,
+      'local-' + name,
+      Devel.current() + '-' + name,
+      name,
+    ];
+  },
+
+  is: function(name) {
+    var searching = this.searchings(name);
+
+    for (var search in searching) {
+      if (this.options[searching[search]] !== undefined) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  get: function(name) {
+    var searching = this.searchings(name);
+
+    for (var search in searching) {
+      if (this.options[searching[search]] !== undefined) {
+        return this.options[searching[search]];
+      }
+    }
+    return false;
+  },
+
+};
